59e2f4a51c0c553fcaa63c9486aa7262001dd2dd
ConnorBambrough <connorbambi@gmail.com>
2020-07-28 03:23:32 +0100

    randomImage added
    grabs random image from specific subreddit

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import discord
 import urllib.request as req
-
+import os
 from discord.ext import commands
-
+import random
 import praw
 
 token = open("token.txt", "r").read()  # I've opted to just save my token to a text file.
@@ -73,16 +73,57 @@ async def on_message(message):  # event that happens per any message.
             try:
                 subreddit = reddit.subreddit(discordSubreddit)
 
-                for submission in subreddit.hot(limit=5):
+                imageUrls = []
+                for submission in subreddit.hot(limit=100):
+                    if submission.url.endswith('.jpg') or submission.url.endswith('.png'):
+                        imageUrls.append(submission.url)
+
                     if not submission.stickied:
                         discordReceive = {'title': submission.title, 'link': f'https:/www.reddit.com{submission.permalink}'}
                         discordReceive = discordReceive['title'] + '\n' + discordReceive['link']
                         await message.channel.send(discordReceive)
+
+                        discordReceive = imageUrls[random.randint(0, len(imageUrls) - 1)]
+                        req.urlretrieve(discordReceive, 'tempDiscord.jpg')
+                        fullPath = os.path.join(os.getcwd(), 'tempDiscord.jpg')
+                        file = discord.File(fullPath)
+
+                        await message.channel.send(discordReceive)
+                        os.remove('tempDiscord.jpg')
                         break
 
             except Exception as e:
                 print(e)
                 await message.channel.send('This sub is either banned, quarantined, or does not exist.')
+
+
+    #Sends random image from a subreddit.
+    if message.content.startswith('randomImage'):
+        if ' ' in  message.content:
+            discordSubreddit = str(message.content).split(' ')[1]
+
+            try:
+                subreddit = reddit.subreddit(discordSubreddit)
+
+                imageUrls = []
+                for submission in subreddit.hot(limit=100):
+                    if submission.url.endswith('.jpg') or submission.url.endswith('.png'):
+                        imageUrls.append(submission.url)
+
+                discordReceive = imageUrls[random.randint(0,len(imageUrls) - 1)]
+                req.urlretrieve(discordReceive, 'tempDiscord.jpg')
+                fullPath = os.path.join(os.getcwd(), 'tempDiscord.jpg')
+
+                file = discord.File(fullPath)
+                await message.channel.send(file=file)
+
+                os.remove('tempDiscord.jpg')
+
+            except Exception as e:
+                print(e)
+                await message.channel.send('This sub is either banned, quarantined, or does not exist.')
+
+
 
     await bot.process_commands(message)
 
