d8f611a625edb316e4d379a492e9f29a1d015af2
ConnorBambrough <connorbambi@gmail.com>
2020-07-28 01:42:08 +0100

    testing praw

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 import discord
 
 from discord.ext import commands
-import random
-import os
 
-import asyncio
+import praw
 
 token = open("token.txt", "r").read()  # I've opted to just save my token to a text file.
 
+reddit = praw.Reddit(client_id="ZPKPPTv0oiUbOQ",
+                     client_secret="lzjMIcEG5FzGls5sZ4DuQsgNnD4",
+                     user_agent="Connors TomodachAI scrawling reddit")
 
 def get_prefix(client, message):
 
@@ -47,13 +48,13 @@ async def on_message(message):  # event that happens per any message.
     print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
     sentdex_guild = bot.get_guild(729816023938760712)
 
-    if "sentdebot.member_count" == message.content.lower():
+    if "tomodachi.member_count" == message.content.lower():
         await message.channel.send(f"```{sentdex_guild.member_count}```")
 
-    elif "sentdebot.logout()" == message.content.lower():
+    elif "tomodachi.logout()" == message.content.lower():
         await bot.close()
 
-    elif "sentdebot.community_report()" == message.content.lower():
+    elif "tomodachi.community_report()" == message.content.lower():
         online, idle, offline = community_report(sentdex_guild)
         await message.channel.send(f"```Online: {online}.\nIdle/busy/dnd: {idle}.\nOffline: {offline}```")
 
@@ -76,6 +77,11 @@ def community_report(guild):
             idle += 1
 
     return online, idle, offline
+
+@bot.commands()
+async def top10(ctx):
+    for submission in reddit.subreddit("dankmemes").hot(limitt=10):
+        await ctx.channel.send(submission.title)
 
 
 bot.run(token, bot=True, reconnect=True)  # recall my token was saved!
